bea84ed68d17a739b51832171bf08d9d4150cc1f
Triloxyy <47944890+Triloxyy@users.noreply.github.com>
2020-08-09 17:03:13 +0300

    a

---
+++ X1/X1.1.py
@@ -0,0 +1,31 @@
+a = input("Üçgen mi dörtgen mi?")
+if (a == "üçgen" or a == "Üçgen") :
+    b = float(input("Birinci kenarı giriniz: "))
+    c = float(input("İkinci kenarı giriniz: "))
+    d = float(input("Üçüncü kenarı giriniz: "))
+    if d<0 or b<0 or c<0:
+        print("Kenar uzunluğu negatif olamaz!")
+    elif not (b+d>=c>b-d and c+d>=b>=c-d and b+c>=d>=b-c):
+        print("Üçgen değildir!")
+    elif (b == c == d):
+        print("Eşkenar üçgen")
+    elif (d==b or d==c or b==c):
+        print("İkizkenar üçgen")
+    else:
+        print("Normal üçgen")
+elif (a == "dörtgen" or a == "Dörtgen"):
+    b = float(input("Birinci kenarı giriniz: "))
+    c = float(input("İkinci kenarı giriniz: "))
+    d = float(input("Üçüncü kenarı giriniz: "))
+    f = float(input("Dördüncü kenarı giriniz: "))
+    if d<0 or b<0 or c<0 or f<0:
+        print("Kenar uzunluğu negatif olamaz!")
+    elif (f == b == c == d):
+        print("Kare")
+    elif f==c and b == d:
+        print("Dikdörtgen")
+    else:
+        print("Dörtgen")
+
+else:
+    print("Anlayamadım")
